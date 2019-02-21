591a9263ae4cbacc688890201a86b9d68403e322
gregory libert <grego525@hotmail.com>
2019-02-22 00:17:31 +0100

    Encoding Form: both langages encoding completed. Add word to list button needs to be done.

--- TranslationWPF/TranslationWPF/ViewModel/EncodingVM.cs
+++ TranslationWPF/TranslationWPF/ViewModel/EncodingVM.cs
@@ -13,83 +13,119 @@ namespace TranslationWPF.ViewModel
 {
     public class EncodingVM: INotifyPropertyChanged
     {
+        #region Propertychanged
         public event PropertyChangedEventHandler PropertyChanged;
         protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+        #endregion
 
+        #region Properties
         public Language Word { get; set; }
         public Language Translation { get; set; }
 
-        public Language.Types[] Types
+        private string wordAddingSynonym ="";
+        public string WordAddingSynonym
         {
-            get
-            {
-                return Word.GetTypesAvailables();
-            }
+            get { return wordAddingSynonym;}
+            set { wordAddingSynonym = value; OnPropertyChanged("WordAddingSynonym");}
         }
+
+        private string translationAddingSynonym="";
+        public string TranslationAddingSynonym
+        {
+            get { return translationAddingSynonym; }
+            set { translationAddingSynonym = value; OnPropertyChanged("TranslationAddingSynonym"); }
+        }
+
+        public Language.Types[] WordTypes
+        {
+            get { return Word.GetTypesAvailables();}
+        }
+        public Language.Types[] TranslationTypes
+        {
+            get { return Translation.GetTypesAvailables(); }
+        }
+
+        private Language.Types wordSelectedType;
+        public Language.Types WordSelectedType
+        {
+            get { return wordSelectedType; }
+            set { wordSelectedType = value; OnPropertyChanged("WordSelectedType"); }
+        }
+
+        private Language.Types translationSelectedType;
+        public Language.Types TranslationSelectedType
+        {
+            get { return translationSelectedType; }
+            set { translationSelectedType = value; OnPropertyChanged("TranslationSelectedType"); }
+        }
+
 
         public ObservableCollection<string> OriginalWordSynonyms { get; set; } = new ObservableCollection<string>();
         public ObservableCollection<string> TranslatedWordSynonyms { get; set; } = new ObservableCollection<string>();
+        #endregion
 
-        private Language.Types selectedType;
-        public Language.Types SelectedType
-        {
-            get { return selectedType; }
-            set { selectedType = value; OnPropertyChanged("SelectedType"); }
-        }
-
+        #region Constructors
         public EncodingVM(Language word, Language translation)
         {
             Word = word;
             Translation = translation;
         }
+        #endregion
 
         #region Commands
-        private CommandHandlerWithParameter _removeCommand;
-        public CommandHandlerWithParameter RemoveCommand
+        private CommandHandlerWithParameter _removeWordCommand;
+        public CommandHandlerWithParameter RemoveWordCommand
         {
-            get { return _removeCommand ?? (_removeCommand = new CommandHandlerWithParameter(item => RemoveHandler((string)item), true)); }
+            get { return _removeWordCommand ?? (_removeWordCommand = new CommandHandlerWithParameter(item => RemoveWordHandler((string)item), true)); }
+        }
+
+        private CommandHandlerWithParameter _removeTranslationCommand;
+        public CommandHandlerWithParameter RemoveTranslationCommand
+        {
+            get { return _removeTranslationCommand ?? (_removeTranslationCommand = new CommandHandlerWithParameter(item => RemoveTranslationHandler((string)item), true)); }
         }
 
         private CommandHandlerWithParameter _addCommand;
         public CommandHandlerWithParameter AddCommand
         {
-            get { return _addCommand ?? (_addCommand = new CommandHandlerWithParameter(item => AddHandler((string)item), true)); }
+            get { return _addCommand ?? (_addCommand = new CommandHandlerWithParameter((item) => AddHandler((string) item), true)); }
             
         }
 
-        private CommandHandlerWithParameter _selectTypeCommand;
-
-       
-
-        public CommandHandlerWithParameter SelectTypeCommand
-        {
-            get { return _selectTypeCommand ?? (_selectTypeCommand = new CommandHandlerWithParameter(item => SelectTypeHandler((Language.Types)item), true)); }
-        }
         #endregion
 
-        void RemoveHandler(string item)
+        #region Methods
+        void RemoveWordHandler(string item)
         {
-            string itemToRemove = OriginalWordSynonyms.Single(w => w == item);
+            string itemToRemove = OriginalWordSynonyms.First(w => w == item);
             OriginalWordSynonyms.Remove(itemToRemove);
-            //foreach (var word in OriginalWordSynonyms)
-            //{
-            //    if (word == item)
-            //        OriginalWordSynonyms.Remove(item);
-            //}
         }
-
-        void AddHandler(string word)
+        void RemoveTranslationHandler(string item)
         {
-            OriginalWordSynonyms.Add(word);
+            string itemToRemove = TranslatedWordSynonyms.First(w => w == item);
+            TranslatedWordSynonyms.Remove(itemToRemove);
         }
-
-        void SelectTypeHandler(Language.Types type)
+        void AddHandler(string item)
         {
-            SelectedType = type;
+            switch (item)
+            {
+                case "1":
+                    if (!string.IsNullOrEmpty(WordAddingSynonym))
+                        OriginalWordSynonyms.Add(WordAddingSynonym);
+                    break;
+                case "2":
+                    if (!string.IsNullOrEmpty(TranslationAddingSynonym))
+                        TranslatedWordSynonyms.Add(TranslationAddingSynonym);
+                    break;
+                default:
+                    break;
+            }
+            WordAddingSynonym = "";
+            TranslationAddingSynonym = "";
         }
-
+        #endregion
     }
 }
--- TranslationWPF/TranslationWPF/Views/EncondingUC.xaml.cs
+++ TranslationWPF/TranslationWPF/Views/EncondingUC.xaml.cs
@@ -24,5 +24,7 @@ namespace TranslationWPF.Views
         {
             InitializeComponent();
         }
+
+
     }
 }
