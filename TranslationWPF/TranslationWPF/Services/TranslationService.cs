b558872138168cc9f63dc0d6c17cc74b6e2ea131
Gregory <grego525@hotmail.com>
2019-09-13 13:58:19 +0200

    Declaring TranslationService in ressource.xaml

--- TranslationWPF/TranslationWPF/Services/TranslationService.cs
+++ TranslationWPF/TranslationWPF/Services/TranslationService.cs
@@ -5,7 +5,6 @@ using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
 using TranslationWPF.Model;
 using TranslationWPF.ViewModel;
 
